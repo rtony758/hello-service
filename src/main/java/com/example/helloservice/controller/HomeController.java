c4020486773c8857c1204822b3eb967030358975
rtony758 <rtony7588@gamil.com>
2018-07-06 10:45:26 +0800

    Initial commit

---
+++ src/main/java/com/example/helloservice/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.example.helloservice.controller;
+
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){return "hello world";}
+}
